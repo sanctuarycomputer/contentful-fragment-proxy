1c6f4598b510588f3f374e46e7420a7813072e6d
Hugh Francis <hugh@sanctuary.computer>
2019-02-07 12:24:01 -0500

    No cache

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 
 module.exports = async function (req, res) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.statusCode = 200;
   return axios({
     method: 'get',
