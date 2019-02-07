f6bd0d51fcd8ff92b4464548c672258eb5e08679
Hugh Francis <hugh@sanctuary.computer>
2019-02-07 12:45:19 -0500

    Fix

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const axios = require("axios");
+const request = require("request");
 
 module.exports = async function (req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-  res.statusCode = 200;
-  return axios({
-    method: 'get',
-    url: 'https://cdn.jsdelivr.net/gh/sanctuarycomputer/contentful-fragment@master/dist/index.html',
-    responseType: 'stream'
-  }).then(({ data }) => data);
+  request("https://cdn.jsdelivr.net/gh/sanctuarycomputer/contentful-fragment@master/dist/index.html")
+  .on("response", remote => {
+    remote.headers['Content-Type'] = 'text/html; charset=utf-8';
+    remote.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+    remote.headers['Pragma'] = 'no-cache';
+    remote.headers['Expires'] = 0;
+  }).pipe(res);
 }
